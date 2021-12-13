4615ad125064f3f68e34b4d3050e87d5f8c9dfa1
ioanaS16 <stefanioana16@gmail.com>
2021-12-13 14:49:22 +0200

    Lab10 13.12

--- Stefan_Ioana_Lab10/App.xaml.cs
+++ Stefan_Ioana_Lab10/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Stefan_Ioana_Lab10.Data;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +8,28 @@ namespace Stefan_Ioana_Lab10
 {
     public partial class App : Application
     {
+
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if(database==null)
+                {
+                    database = new 
+                    ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new ListEntryPage());
         }
 
         protected override void OnStart()
